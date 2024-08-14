3759900be7fdbb39d963f87fe48cf994ddbd7f1d
vereto <vr.popova@gmail.com>
2024-08-14 17:32:07 +0300

    async function

--- AsyncFunctions.js
+++ AsyncFunctions.js
@@ -1,3 +1,15 @@
  const login = async(username, password)=>{
     if(!username || !password) throw 'Missing Credentials'
- }
+    if (password === 'dayandnight'){ return'WELCOME'}
+      throw ("Invalid Password");
+    }
+
+    login('hdtdtghd')
+    .then(msg => {
+      console.log('USER LOGGED IN');
+      console.log(msg);
+    })
+    .catch(err => {
+      console.log('ERROR');
+      console.log(err);
+    })
