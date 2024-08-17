da10bcf95cfcf0204f6fce7f4be392a73fba1d0f
vereto <vr.popova@gmail.com>
2024-08-17 17:17:07 +0300

    fetch.then.catch

--- usingFetchAPI.js
+++ usingFetchAPI.js
@@ -1,6 +1,18 @@
 fetch("https://swapi.dev/api/people/1")
-.then(res =>{
+.then((res) =>{
     console.log('RESOLVED!',res);
+   return res.json()  
+})
+.then((data) => {
+    console.log(data)
+    return fetch ("https://swapi.dev/api/people/2")
+})
+.then((res) => {
+    console.log('SECOND REQUEST RESOLVED!!!');
+    return res.json()    
+})
+.then((data) => {
+    console.log(data);
 })
 .catch((e)=>{
     console.log('error!', e);
