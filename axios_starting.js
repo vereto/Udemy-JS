db5e7a877b04d938a32ef6aa1b41cca87b039827
vereto <vr.popova@gmail.com>
2024-08-18 17:21:38 +0300

    AXIOS BEGINNING

---
+++ axios_starting.js
@@ -0,0 +1,20 @@
+// axios.get ("https://swapi.dev/api/people/1").then((res)=>{
+//     console.log('RESPONSE:',res);
+// })
+// .catch((e)=>{
+//     console.log('ERROR:',e);
+    
+// })
+const getStarWarsPerson = async (id) => {
+    try {
+         const res = await axios.get(`https://swapi.dev/api/people/${id}/`)
+    console.log(res.data);
+        
+    } catch (error) {
+        console.log('ERROR');
+        
+    }
+   
+    
+}
+getStarWarsPerson(5)
