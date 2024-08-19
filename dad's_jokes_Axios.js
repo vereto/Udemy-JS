d7bfd836018a8753d551d7e5a84161ec096bf3e5
vereto <vr.popova@gmail.com>
2024-08-19 19:41:04 +0300

    setting headers with axios

--- dad's_jokes_Axios.js
+++ dad's_jokes_Axios.js
@@ -1,17 +1,25 @@
-const fetchBitcoinPrice = async () => {
-  try {
-    const res = await axios.get("http://api.cryptonator.com");
-    console.log(res.data.ticker.price);
-  } catch (e) {
-    console.log("ERROR!", e);
-  }
-};
-
+// const fetchBitcoinPrice = async () => {
+//   try {
+//     const res = await axios.get("http://api.cryptonator.com");
+//     console.log(res.data.ticker.price);
+//   } catch (e) {
+//     console.log("ERROR!", e);
+//   }
+// };
 const jokes = document.querySelector('#jokes')
+const button = document.querySelector('#button')
+
+const addNewJoke = async() =>{
+    
+const jokeText = await getDadJoke();
+const newLI = document.createElement('LI');
+newLI.append(jokeText)
+jokes.append(newLI)
+}
+
 const getDadJoke = async () => {
     const config =  {headers: { Accept:'application/json'}}
     const res = await axios.get('https://icanhazdadjoke.com/',config);
-    const newLI = document.createElement('LI');
-    newLI.append(res.data.joke)
-    jokes.append(newLI)
+    return res.data.joke;
 }
+button.addEventListener('click', addNewJoke)
