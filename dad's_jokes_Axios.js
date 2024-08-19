22bd5a55fb2e888dd3bfaf47fd47eb53390203ec
vereto <vr.popova@gmail.com>
2024-08-19 19:53:18 +0300

    setting headers using Axios

--- dad's_jokes_Axios.js
+++ dad's_jokes_Axios.js
@@ -18,8 +18,13 @@ jokes.append(newLI)
 }
 
 const getDadJoke = async () => {
-    const config =  {headers: { Accept:'application/json'}}
-    const res = await axios.get('https://icanhazdadjoke.com/',config);
-    return res.data.joke;
+    try {
+        const config =  {headers: { Accept:'application/json'}}
+        const res = await axios.get('https://icanhazdadjoke.com/',config);
+        return res.data.joke;
+    } catch (error) {
+        return 'NO JOKES HERE'
+    }
+   
 }
 button.addEventListener('click', addNewJoke)
