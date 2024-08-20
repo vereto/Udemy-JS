432f84de21430a64543ef78478aeb1452a9a22fa
vereto <vr.popova@gmail.com>
2024-08-20 21:39:44 +0300

    axios.get - search and clear img from app

--- tvShow_search_app.js
+++ tvShow_search_app.js
@@ -1,4 +1,29 @@
-const form = document.querySelector('#SearchForm')
-form.addEventListener('submit', function (e) {
-     e.preventDefault()   
-})
+const form = document.querySelector("#SearchForm");
+
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  const searchTerm = form.elements.query.value;
+  const config = {params: {q: searchTerm}}
+  const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+  deleteExistingImg()
+  makeImg(res.data)
+  form.elements.query.value = ''
+  
+});
+
+const makeImg = (shows) => {
+
+  for (let result of shows) {
+   
+    if (result.show.image) {
+    const img = document.createElement("IMG");
+    img.src = result.show.image.medium;
+    document.body.append(img);
+}
+  }
+};
+
+const deleteExistingImg = () => {
+const images = document.querySelectorAll('img')
+images.forEach(img => img.remove());
+}
