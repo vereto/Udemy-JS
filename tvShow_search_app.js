ec78371397852073c7412fffefc47adf0fcf55b6
vereto <vr.popova@gmail.com>
2024-08-21 17:04:04 +0300

    axios in search app(create & remove imgs)

--- tvShow_search_app.js
+++ tvShow_search_app.js
@@ -18,26 +18,27 @@ const makeImg = (shows) => {
     if (result.show.image) {
     const img = document.createElement("IMG");
     img.src = result.show.image.medium;
-    document.body.append(img);
+    document.body.append(img)
 }
   }
 };
+const deleteExistingImg = () => {
+    const images = document.querySelectorAll('img')
+    images.forEach(img => img.remove())
+}
 
 // const deleteExistingImg = () => {
 // const images = document.querySelectorAll('img')
 // images.forEach(img => img.remove());
 // }
 
-//removing by childNodes
-const deleteExistingImg = () => {
-const bodyChildren = document.body.childNodes;
-for (let i = bodyChildren.length - 1; i >= 0; i--) {
-    if (bodyChildren[i].nodeName === 'IMG') {
-        document.body.removeChild(bodyChildren[i])
-    } 
-}
-}
-
+// //removing by childNodes
 // const deleteExistingImg = () => {
-
+// const bodyChildren = document.body.childNodes;
+// for (let i = bodyChildren.length - 1; i >= 0; i--) {
+//     if (bodyChildren[i].nodeName === 'IMG') {
+//         document.body.removeChild(bodyChildren[i])
+//     } 
 // }
+// }
+
