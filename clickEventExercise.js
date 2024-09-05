bf24859562977a570cf77ea7933c50fe78f7a118
vereto <vr.popova@gmail.com>
2024-09-05 14:46:00 +0300

    events

---
+++ clickEventExercise.js
@@ -0,0 +1,4 @@
+const helloBtn = document.querySelector('#hello');
+helloBtn.addEventListener('click', function(){console.log('hello')})
+const goodBtn = document.querySelector('#goodbye')
+goodBtn.addEventListener('click', function(){console.log('goodbye')})
