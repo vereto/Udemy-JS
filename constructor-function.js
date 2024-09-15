51f2e1986eeea3456408e3d0e5f532cf6b39319e
vereto <vr.popova@gmail.com>
2024-09-15 21:37:32 +0300

    constructor function

--- constructor-function.js
+++ constructor-function.js
@@ -9,7 +9,7 @@ Color.prototype.rgb = function () {
 }
 Color.prototype.hex = function () {
     const {r,g,b} = this;
-    return '#' + ((1 << 24) + (r << 16) + (g << 8).toString(16).slice(1))
+    return ('#' + ((1 << 24) + (r << 16) + (g << 8)+ b).toString(16).slice(1))
 }
 Color.prototype.rgba = function (a = 1.0) {
     const {r, g, b} = this;
