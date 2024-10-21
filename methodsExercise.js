93deebfc601b972f92ac4ca5bc526dcd77119a5e
vereto <vr.popova@gmail.com>
2024-10-21 13:00:43 +0300

    HTML exercise

---
+++ methodsExercise.js
@@ -0,0 +1,10 @@
+const square = {
+    area: function(side){
+        return side * side
+        
+    },
+    perimeter: function (side){
+        return side * 4
+    }
+}
+console.log(square.area(8));
